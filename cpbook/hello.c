3bad31acf95c782f48b24030ff76862c135b9f4c
Ziaur Abdirahman <adtechzia@gmail.com>
2019-09-08 21:27:20 +0600

    first commit

---
+++ cpbook/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+  printf("Hello World\n");
+  printf("I love my country Bangladesh\n");
+  return 0;
+}
---
+++ cpbook/sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  int a;
+  int b;
+  int sum;
+  a = 50;
+  b = 60;
+  sum = a + b;
+  printf("Sum is %d\n", sum);
+  return 0;
+}
